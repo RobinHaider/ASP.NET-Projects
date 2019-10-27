c59b71463a05b61d0c283a49899dcea37c08ccab
Robin Haider <robinhaider69@gmail.com>
2019-10-27 22:04:35 +0600

    PSMS2 Commit-9 Class Index with viewModel

---
+++ Primary School Management System - 2/Primary School Management System - 2/Controllers/ClassController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Primary_School_Management_System___2.DAL;
+using Primary_School_Management_System___2.View_Model;
+
+namespace Primary_School_Management_System___2.Controllers
+{
+    public class ClassController : Controller
+    {
+        SchoolDbContext db = new SchoolDbContext();
+
+        // GET: Class
+        public ActionResult Index()
+        {
+            List<ClassVM> classVms = new List<ClassVM>();
+            var classes = db.Classes
+                .Include(s => s.Subjects)
+                .Include(s => s.Teacher)
+                .ToList();
+
+            foreach (var @class in classes)
+            {
+                ClassVM classVm = new ClassVM();
+
+                classVm.Class = @class.ClassName;
+                classVm.ClassTeacher = @class.Teacher.Name;
+                @classVm.Subjects = new List<string>();
+                @class.Subjects.ForEach(s=> classVm.Subjects.Add(s.Name));
+                classVm.TotalStudent = db.Students
+                    .Count(s => s.ClassID == @class.ID);
+
+                classVms.Add(classVm);
+            }
+
+            
+
+            return View(classVms);
+        }
+    }
+}
---
+++ Primary School Management System - 2/Primary School Management System - 2/View Model/ClassVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Primary_School_Management_System___2.View_Model
+{
+    public class ClassVM
+    {
+        public string Class { get; set; }
+        [Display(Name = "Class Teacher")]
+        public string ClassTeacher { get; set; }
+        public List<string> Subjects { get; set; }
+        [Display(Name = "Total Student")]
+        public int TotalStudent { get; set; }
+
+    }
+}
