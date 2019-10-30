76a62042ce9a9db1a3a4939d6cd67ec46414e3b4
Robin Haider <robinhaider69@gmail.com>
2019-10-30 20:32:02 +0600

    PSMS2 Commit-11 Single course result & cascading dropdown

--- Primary School Management System - 2/Primary School Management System - 2/Controllers/ResultController.cs
+++ Primary School Management System - 2/Primary School Management System - 2/Controllers/ResultController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Primary_School_Management_System___2.DAL;
+using Primary_School_Management_System___2.Models;
 using Primary_School_Management_System___2.View_Model;
 
 namespace Primary_School_Management_System___2.Controllers
@@ -120,93 +121,46 @@ namespace Primary_School_Management_System___2.Controllers
         }
 
 
-        //For all Student Result.....
-        //public ActionResult Index()
-        //{
-        //    List<ResultVM> resultVms = new List<ResultVM>();
+        public ActionResult SingleCourseIndex(int? SelectedClass, int? SelectedSubject, int? SelectedExamType)
+        {
+            var classes = db.Classes.ToList();
+            ViewBag.SelectedClass = new SelectList(classes, "ID", "ClassName", SelectedClass);
 
-        //    var students = db.Students.ToList();
+            if (SelectedSubject != null)
+            {
+                var subjects = db.Subjects.Where(s=> s.ClassID==SelectedClass).ToList();
+                ViewBag.SelectedSubject = new SelectList(subjects, "ID", "Name", SelectedSubject);
+            }
+            else
+            {
+                ViewBag.SelectedSubject = new SelectList(Enumerable.Empty<SelectListItem>());
+            }
 
-        //    foreach (var student in students)
-        //    {
-        //        ResultVM resultVm = new ResultVM();
+            var examTypes = db.ExamTypes.ToList();
+            ViewBag.SelectedExamType = new SelectList(examTypes, "ID", "TypeName", SelectedExamType);
 
-        //        resultVm.Class = student.Class.ClassName;
-        //        resultVm.RollNo = student.RollNo;
-        //        resultVm.Name = student.Name;
+            List<Result> subjectResults = new List<Result>();
 
-        //        var subjects = db.Subjects.Where(s => s.ClassID == student.ClassID).ToList();
+            if (SelectedClass != null && SelectedSubject != null && SelectedExamType != null)
+            {
+                subjectResults = db.Results
+                    .Where(s => s.SubjectID == SelectedSubject && s.ExamTypeID == SelectedExamType)
+                    .OrderBy(s => s.Student.RollNo).ToList();
+            }
 
-        //        foreach (var subject in subjects)
-        //        {
-        //            for (int i = 1; i <= 2; i++)
-        //            {
-        //                var result = db.Results.FirstOrDefault(s =>
-        //                s.StudentID == student.ID && s.SubjectID == subject.ID && s.ExamTypeID == i);
-        //                string subjectName = db.Subjects.Single(s => s.ID == result.SubjectID).Name;
+            
+            return View(subjectResults);
+        }
 
-        //                resultVm.ExamType = i == 1 ? "Half Yearly" : "Final";
+        [AcceptVerbs(HttpVerbs.Get)]
+        public JsonResult GetSubjectByClass(int clsID)
+        {
+            var subjectList = db.Subjects.Where(s => s.ClassID == clsID).ToList();
 
-        //                switch (subjectName)
-        //                {
-        //                    case "আমার বাংলা বই":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.Bangla = (int)result.Number;
-        //                        break;
-        //                    case "প্রাথমিক গণিত":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.Math = (int)result.Number;
-        //                        break;
-        //                    case "English For Today":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.English = (int)result.Number;
-        //                        break;
-        //                    case "প্রাথমিক বিজ্ঞান":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.GeneralScience = (int)result.Number;
-        //                        break;
-        //                    case "বাংলাদেশ ও বিশ্বপরিচয়":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.SocialScience = (int)result.Number;
-        //                        break;
-        //                    case "ইসলাম ও নৈতিক শিক্ষা":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.IslamicEdication = (int)result.Number;
-        //                        break;
-        //                    case "হিন্দুধর্ম ও নৈতিক শিক্ষা":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.HinduismEducation = (int)result.Number;
-        //                        break;
-        //                    case "বৌদ্ধধর্ম ও নৈতিক শিক্ষা":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.BuddhismEducation = (int)result.Number;
-        //                        break;
-        //                    case "খ্রিষ্টধর্ম ও নৈতিক শিক্ষা":
-        //                        if (result != null)
-        //                            if (result.Number != null)
-        //                                resultVm.ChristianityEducation = (int)result.Number;
-        //                        break;
-        //                }
-        //                resultVms.Add(resultVm);
-        //            }
+            var selectedSubject = new SelectList(subjectList, "ID", "Name");
 
-        //        }
-
-
-
-        //    }
-        //    return View(resultVms);
-        //}
-
-
+            return Json(selectedSubject, JsonRequestBehavior.AllowGet);
+        }
 
     }
 }
